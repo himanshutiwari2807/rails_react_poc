22303bc4ae4c6da870a1016227848d07a191b556
Pawan vedpathak <pawan.vedpathak@arieotech.com>
2024-07-29 22:02:28 +0530

    course changes done

--- app/javascript/react/src/CoursePlayer/CourseSidebar.jsx
+++ app/javascript/react/src/CoursePlayer/CourseSidebar.jsx
@@ -1,21 +1,42 @@
-import React  from "react";
+import React from "react";
 
-
-const CourseSideBar = () =>{
-
-    return(
-        <>
-
-        <div class="offcanvas offcanvas-start show" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">IBM: Introduction to Web Development with HTML5, CSS3, and JavaScript</h5>
-          </div>
-          <div class="offcanvas-body">
-            <p>Model Completed.</p>
-          </div>
+const CourseSideBar = () => {
+  return (
+    <>
+      <div
+        class="offcanvas offcanvas-start show course-sidebar"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+            IBM: Introduction to Web Development with HTML5, CSS3, and
+            JavaScript
+          </h5>
         </div>
-        </>
-    )
+        <div class="offcanvas-body">
+          <p>Model Completed.</p>
+
+          <ul>
+            <li>
+              <button>Content</button>
+            </li>
+            <li>
+              <button>Video</button>
+            </li>
+            <li>
+              <button>Audio</button>
+            </li>
+            <li>
+              <button>Quize</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CourseSideBar;
