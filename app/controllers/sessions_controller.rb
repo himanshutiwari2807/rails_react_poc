addf68e6f68c6a9afac02b71452f9918b4d3d4e9
Pawan Vedpathak <pawan.vedpathak@arieotechs-Laptop.local>
2024-08-06 11:24:41 +0530

    login changes done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   # skip_before_action :authenticate_request
-
+  skip_before_action :verify_authenticity_token
   def create
     user = User.find_by(email: params[:email])
 
